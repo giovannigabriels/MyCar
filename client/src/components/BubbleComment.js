88e32288ed06c9829f11cd912b907eba0d98a01f
Giovanni Gabriel Septriadi <giovanni@eigen.co.id>
2023-08-21 04:31:48 +0700

    add feature real time comment

--- client/src/components/BubbleComment.js
+++ client/src/components/BubbleComment.js
@@ -1,16 +1,14 @@
 import { diffTime } from "../helper";
 
-export default function BubbleComment({ comment}) {
-    // console.log(comment,"comment");
-const diffTimee = diffTime(comment)
-    return (
-        <div className="chat chat-start">
-        <div className="chat-header">
-          {comment.User.name}
-          <time className="text-xs opacity-50">{diffTimee}</time>
-        </div>
-        <div className="chat-bubble">{comment.text}</div>
+export default function BubbleComment({ comment }) {
+  const diffTimee = diffTime(comment);
+  return (
+    <div className="chat chat-start">
+      <div className="chat-header">
+        {comment.User.name}
+        <time className="text-xs opacity-50">{diffTimee}</time>
       </div>
-    );
-  }
-  
+      <div className="chat-bubble">{comment.text}</div>
+    </div>
+  );
+}
--- client/src/components/DetailPage.js
+++ client/src/components/DetailPage.js
@@ -2,21 +2,55 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchOneCars } from "../store/actions/carAction";
-import { fetchCarComment } from "../store/actions/carCommentAction";
+import {
+  addCarComment,
+  carCommentFetch,
+  fetchCarComment,
+} from "../store/actions/carCommentAction";
 import BubbleComment from "./BubbleComment";
+import io from "socket.io-client";
+const socket = io("http://localhost:3000");
 
 export default function DetailPage() {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const { carComment } = useSelector((state) => state.carCommentReducer);
+
+  const [comments, setComments] = useState([]);
+
   let [loading, setLoading] = useState(true);
+  let [inputComment, setInputComment] = useState({
+    text: "",
+    carId: id,
+  });
   let [input, setInput] = useState({
     name: "",
     price: "",
     imgUrl: "",
     author: "",
-    description: ""
+    description: "",
   });
+  const handleSubmitComment = (ev) => {
+    ev.preventDefault();
+    const names = localStorage.getItem("name");
+    socket.emit("comment", {
+      text: inputComment.text,
+      carId: id,
+      User: { name: names },
+      createdAt: new Date(),
+    });
+    dispatch(addCarComment(inputComment));
+    setInputComment({
+      text: "",
+      carId: id,
+    });
+  };
+
+  const handleChange = (ev) => {
+    setInputComment({
+      ...inputComment,
+      [ev.target.name]: ev.target.value,
+    });
+  };
   useEffect(() => {
     //fetch data
     dispatch(fetchOneCars(id))
@@ -26,7 +60,7 @@ export default function DetailPage() {
           price: data.price,
           imgUrl: data.carPicture,
           author: data.User.name,
-          description: data.description
+          description: data.description,
         });
       })
       .catch((error) => {
@@ -35,69 +69,93 @@ export default function DetailPage() {
           error
         );
       })
-      .finally(()=>{
+      .finally(() => {
         setLoading(false);
+      });
+    dispatch(fetchCarComment(id))
+      .then((data) => {
+        setComments(data);
       })
-      dispatch(fetchCarComment(id))
-      .finally(()=>{
-       setLoading(false)
-       });
+      .finally(() => {
+        setLoading(false);
+      });
+
+    socket.on("comment", (comment) => {
+      if (comment.carId === id) {
+        setComments((prevComments) => [...prevComments, comment]);
+        console.log(comment, "masukk socket comment ONN");
+      }
+    });
   }, []);
   return (
     <div>
+      ß
       <div className="bg-yellow-500">
         <p className="  font-bold text-2xl text-white text-center">
           {input.name}
         </p>
       </div>
       <div className="mx-96 my-10">
-        {
-          loading === false ? (        
+        {loading === false ? (
           <div className="card lg:card-side bg-base-100 shadow-xl">
-          <figure className=" ml-5">
-            <img
-              src={input.imgUrl}
-              alt="Album"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title  text-red-600 font-semibold">
-              {" "}
-              {new Intl.NumberFormat("id-ID", {
-                style: "currency",
-                currency: "IDR",
-              }).format(input.price)}
-            </h2>
-            <span>Description : {input.description}</span>
-            <h2 className=" font-bold">Author : {input.author}</h2>
-            <div className="card-actions justify-end">
-              <Link
-                to={"/"}
-                className="btn btn-primary">
-                Back to Home
-              </Link>
+            <figure className=" ml-5">
+              <img
+                src={input.imgUrl}
+                alt="Album"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title  text-red-600 font-semibold">
+                {" "}
+                {new Intl.NumberFormat("id-ID", {
+                  style: "currency",
+                  currency: "IDR",
+                }).format(input.price)}
+              </h2>
+              <span>Description : {input.description}</span>
+              <h2 className=" font-bold">Author : {input.author}</h2>
+              <div className="card-actions justify-end">
+                <Link
+                  to={"/"}
+                  className="btn btn-primary">
+                  Back to Home
+                </Link>
+              </div>
             </div>
           </div>
-        </div>) : (
-        <progress className="progress w-96"></progress>
-        )
-        }
+        ) : (
+          <progress className="progress w-96"></progress>
+        )}
       </div>
       <div className="mx-96 my-10">
-        {
-          console.log(carComment,"ini carcomment")
-        }
-        {
-          carComment.map((data)=>{
-            return (
-              <BubbleComment
+        {comments.map((data) => {
+          return (
+            <BubbleComment
               comment={data}
               key={data.id}
-              />
-            )
-          })
-        }
+            />
+          );
+        })}
+      </div>
+      <div>
+        <form
+          onSubmit={handleSubmitComment}
+          className="mx-96 my-10 flex flex-row">
+          <div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs mt-5"
+              value={inputComment.text}
+              onChange={handleChange}
+              name="text"
+            />
+          </div>
 
+          <div className="form-control mx-5 my-5">
+            <button className="btn btn-secondary">comments</button>
+          </div>
+        </form>
       </div>
     </div>
   );
